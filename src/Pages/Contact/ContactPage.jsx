b47c7b1678066af8d85d595358f41545754243ac
mikun <timilehinonifara@gmail.com>
2025-10-08 03:15:07 -0700

    updated

---
+++ src/Pages/Contact/ContactPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import GetInTouch from "../../Components/Pages/Home/GetInTouch/GetInTouch";
+import ContactHero from "../../Components/Pages/Contact/ContactHero/ContactHero";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <div>
+        <ContactHero />
+        <GetInTouch />
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
